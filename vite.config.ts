bca047201a28689972b1225c526c2e8026d74eaf
MellowCo <799478052@qq.com>
2022-06-26 18:17:21 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite'
+import uni from '@dcloudio/vite-plugin-uni'
+import Unocss from 'unocss/vite'
+import preSetWxapp from 'unocss-preset-wxapp'
+
+
+export default defineConfig({
+  plugins: [
+    uni(),
+    Unocss({
+      presets: [
+        preSetWxapp()
+      ],
+      shortcuts: [{
+        'border-base': 'border border-gray-500_10',
+      }],
+    }),
+  ],
+})
