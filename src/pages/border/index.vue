362cd1dc194a8c540107e0929c91155ab3b0eed2
MellowCo <799478052@qq.com>
2022-06-26 19:22:54 +0800

    feat: border

---
+++ src/pages/border/index.vue
@@ -0,0 +1,87 @@
+<!--
+ * @Author: licl
+ * @Date: 2022-06-26 17:28:31
+ * @LastEditTime: 2022-06-26 19:21:55
+ * @LastEditors: licl
+ * @Description:
+-->
+<script setup lang="ts">
+const radius = {
+  title: 'border-radius',
+  classNames: [
+    'rounded-none',
+    'rounded-sm',
+    'rounded',
+    'rounded-md',
+    'rounded-lg',
+    'rounded-xl',
+    'rounded-2xl',
+    'rounded-3xl',
+    'rounded-1_2',
+    'rounded-full',
+  ],
+}
+
+const width = {
+  title: 'border-width',
+  classNames: [
+    'border-0',
+    'border',
+    'border-base',
+    'border-2',
+    'border-4',
+    'border-6',
+    'border-8',
+    'border-10',
+  ],
+}
+
+const style = {
+  title: 'border-style',
+  classNames: [
+    'border-none',
+    'border-solid',
+    'border-dashed',
+    'border-dotted',
+    'border-double',
+  ],
+}
+
+const color = {
+  title: 'border-style',
+  classNames: [
+    'border-transparent border-4',
+    'border-gray-500 border-4',
+    'border-red-500 border-4',
+    'border-yellow-500 border-4',
+    'border-blue-500 border-4',
+    'border-green-500 border-4',
+    'border-gray-500_20 border-4',
+    'border-red-500_20 border-4',
+    'border-yellow-500_20 border-4',
+    'border-blue-500_20 border-4',
+    'border-green-500_20 border-4',
+  ],
+}
+
+const list = [width, radius, style, color]
+</script>
+
+<template>
+  <view class="bg-white">
+    <view v-for="(item, i) in list" :key="i" class="p-2">
+      <view class="text-2xl text-center my-3">
+        {{ item.title }}
+      </view>
+      <view class="flex flex-col items-center">
+        <view v-for="(className, j) in item.classNames" :key="j" :class="className" class="w-half h-200 mb-3  bg-blue-100 center text-center">
+          {{ className }}
+        </view>
+      </view>
+    </view>
+  </view>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -2,8 +2,8 @@
 const list = [
   { name: 'font', path: '/pages/font/index' },
   { name: 'size', path: '/pages/size/index' },
-  { name: 'font', path: '/pages/font/index' },
-  { name: 'font', path: '/pages/font/index' },
+  { name: 'shadow', path: '/pages/shadow/index' },
+  { name: 'border', path: '/pages/border/index' },
 ]
 
 const to = (to: string) => {
@@ -14,38 +14,12 @@ const to = (to: string) => {
 </script>
 
 <template>
-  <view class="grid grid-cols-3 justify-items-center">
-    <view v-for="(item, index) in list" :key="index" class="w-200 h-200 shadow-md flex justify-center items-center rounded-md" @click="to(item.path)">
-      {{ item.name }}
+  <view class="py-3">
+    <view class="grid grid-cols-3 justify-items-center">
+      <view v-for="(item, index) in list" :key="index" class="w-200 h-200 shadow-md center rounded-md" @click="to(item.path)">
+        {{ item.name }}
+      </view>
     </view>
   </view>
 </template>
 
-<style>
-.content {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-.logo {
-  margin-left: auto;
-  margin-right: auto;
-  margin-top: 200rpx;
-  margin-bottom: 50rpx;
-  width: 200rpx;
-  height: 200rpx;
-}
-
-.text-area {
-  display: flex;
-  justify-content: center;
-}
-
-.title {
-  font-size: 36rpx;
-  color: #8f8f94;
-}
-
-</style>
